c8614476836bda444f72e36b6c00f699bd76f894
mga-time <123513681+mga-time@users.noreply.github.com>
2023-01-25 19:44:17 +0200

    Update and rename sendTelegram.php to send2Telegram.php

--- send2Telegram.php
+++ send2Telegram.php
@@ -19,9 +19,9 @@ function parser($url){
 function orderSendTelegram($message) { 
  
     /*токен который выдаётся при регистрации бота */ 
-    $token = "1616052093:AAGZ5vsK12RTT92jj2yHaZ4tZIUB_8N-tY"; 
+    $token = "1616052093:5928262425:AAHiPZw1GP960ol66C2QRNEC57krQ38CTJo"; 
     /*идентификатор группы*/ 
-    $chat_id = "-435154116"; 
+    $chat_id = "-862732580"; 
  
     /*делаем запрос*/ 
     parser("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$message}"); 
